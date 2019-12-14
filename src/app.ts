78a2d7743f2ea3b38e0dec3e2ce10d211a010b9f
Сергей Терехов <sterehov@MacBook-Pro-Sergej.local>
2019-12-14 15:38:38 +0300

    Единая точка запуска

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import indexRouter from "./controllers";
+
+const app = express();
+
+app.use(bodyParser.json({limit: "10mb"}));
+
+app.use("/", indexRouter);
+
+app.listen(3000);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-import express from "express";
-import bodyParser from "body-parser";
-
-import indexRouter from "./controllers";
-
-const app = express();
-
-app.use(bodyParser.json({limit: "10mb"}));
-
-app.use("/", indexRouter);
-
-app.listen(3000);
+import "./app";
+import "./msPush";
