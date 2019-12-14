875053c7770da9830ef5bf7ccbaba8f7e602aa7e
Сергей Терехов <sterehov@MacBook-Pro-Sergej.local>
2019-12-14 15:42:27 +0300

    Улучшен лог

--- src/msPush.ts
+++ src/msPush.ts
@@ -6,7 +6,7 @@ const app = express();
 app.get("/how-are-you", async (req, res) => {
     const number = await services.push.sendHowAreYouAll();
 
-    res.send(`OK: ${number}`);
+    res.send(`OK: ${number}\n`);
 });
 
 app.listen(3500);
