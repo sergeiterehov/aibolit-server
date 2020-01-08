a7b2bdc15fd1864a034ce36bf6d628a43b171fe0
Сергей Терехов <sterehov@MacBook-Pro-Sergej.local>
2020-01-08 13:53:07 +0300

    Улучшено логирование времени

--- src/msPush.ts
+++ src/msPush.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import moment from "moment";
 import { services } from "./services";
 import "./models";
 
@@ -7,7 +8,7 @@ const app = express();
 app.get("/how-are-you", async (req, res) => {
     const number = await services.push.sendHowAreYouAll();
 
-    res.send(`OK: ${number}\n`);
+    res.send(`OK (${moment().toISOString()}): ${number}\n`);
 });
 
 app.listen(3500);
