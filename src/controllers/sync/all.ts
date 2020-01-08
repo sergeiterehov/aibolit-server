feec3c9accd13652fdd79948bcbb8dc2fff4393d
Сергей Терехов <sterehov@MacBook-Pro-Sergej.local>
2020-01-08 13:23:54 +0300

    Добавлены остальные методы для выгрузки

--- src/controllers/sync/all.ts
+++ src/controllers/sync/all.ts
@@ -1,9 +1,18 @@
-import { Router } from "express";
+import { Router, RequestHandler } from "express";
+import moment from "moment";
 import { withErrorHandler } from "../../middlewares/withErrorHandler";
 import { HealthStep } from "../../models/HealthStep";
-import moment from "moment";
+import { telemedDB } from "../../databases/telemed";
+import { HealthHartRate } from "../../models/HealthHartRate";
+import { HealthWeight } from "../../models/HealthWeight";
+import { HealthHeight } from "../../models/HealthHeight";
+import { HealthMassIndex } from "../../models/HealthMassIndex";
 
 function csvValue(raw: any): string {
+    if (raw instanceof Date) {
+        return String(moment(raw).unix());
+    }
+
     switch (typeof raw) {
         case "number":
         case "boolean":
@@ -14,44 +23,108 @@ function csvValue(raw: any): string {
     }
 }
 
-const router = Router();
+function dataSelector(tableName, fields: string[]): RequestHandler {
+    return async function(req, res) {
+        res.setHeader("Content-Type", "application/csv; charset=utf-8");
 
-router.get("/steps", withErrorHandler(async (req, res) => {
-    res.setHeader("Content-Type", "application/csv; charset=utf-8");
+        res.write(fields.join(",") + "\n", "UTF-8");
 
-    res.write([
-        "userId",
-        "device",
-        "periodFrom",
-        "periodTo",
-        "value",
-    ].join(",") + "\n", "UTF-8");
+        let offset = 0;
 
-    let offset = 0;
+        while (true) {
+            const [
+                items,
+            ]: any = await telemedDB.query(
+                `select ${fields.join(",")} from ${tableName} limit ${offset}, 100000`,
+                { raw: true },
+            );
 
-    while (true) {
-        const steps = await HealthStep.findAll({
-            where: {},
-            limit: 10000,
-            offset,
-        });
+            if (!items.length) {
+                break;
+            }
 
-        if (!steps.length) {
-            break;
+            console.log(items);
+
+            offset += items.length;
+
+            res.write(
+                items
+                    .map(item =>
+                        fields
+                            .map(field => item[field])
+                            .map(csvValue)
+                            .join(","),
+                    )
+                    .join("\n") + "\n",
+                "UTF-8",
+            );
         }
 
-        offset += steps.length;
+        res.end();
+    };
+}
 
-        res.write(steps.map((item) => [
-            item.userId,
-            item.device,
-            moment(item.periodFrom).unix(),
-            moment(item.periodTo).unix(),
-            item.val,
-        ].map(csvValue).join(",")).join("\n") + "\n", "UTF-8");
-    }
+const router = Router();
 
-    res.end();
-}));
+router.get(
+    "/steps",
+    withErrorHandler(
+        dataSelector(HealthStep.tableName, [
+            "userId",
+            "device",
+            "periodFrom",
+            "periodTo",
+            "val",
+        ]),
+    ),
+);
+
+router.get(
+    "/hart-rate",
+    withErrorHandler(
+        dataSelector(HealthHartRate.tableName, [
+            "userId",
+            "device",
+            "date",
+            "avgVal",
+        ]),
+    ),
+);
+
+router.get(
+    "/weight",
+    withErrorHandler(
+        dataSelector(HealthWeight.tableName, [
+            "userId",
+            "device",
+            "date",
+            "val",
+        ]),
+    ),
+);
+
+router.get(
+    "/height",
+    withErrorHandler(
+        dataSelector(HealthHeight.tableName, [
+            "userId",
+            "device",
+            "date",
+            "val",
+        ]),
+    ),
+);
+
+router.get(
+    "/mass-index",
+    withErrorHandler(
+        dataSelector(HealthMassIndex.tableName, [
+            "userId",
+            "device",
+            "date",
+            "val",
+        ]),
+    ),
+);
 
 export default router;
