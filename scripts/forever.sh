e0965cd09d8f8bc1d86afdc73b11379e03049522
Сергей Терехов <sterehov@MacBook-Pro-Sergej.local>
2020-01-08 16:41:43 +0300

    start

--- scripts/forever.sh
+++ scripts/forever.sh
@@ -1,1 +1,1 @@
-forever start --uid telemed --spinSleepTime 10000 -c "nodemon --exitcrash" src/index.ts
+forever start --uid telemed --spinSleepTime 10000 --append -c "nodemon --exitcrash" src/index.ts
